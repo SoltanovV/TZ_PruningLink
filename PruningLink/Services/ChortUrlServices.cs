d8e0c8376ed79eca75d7efdcc6fa3259afb49818
VladislavSoltanov <vsoltanovv@gmail.com>
2022-09-26 08:41:45 +0200

    Добавлен метод для генерации рандомного значения

--- PruningLink/Services/ChortUrlServices.cs
+++ PruningLink/Services/ChortUrlServices.cs
@@ -17,8 +17,8 @@ namespace PruningLink.Services
 			try
 			{
                 Uri uri = new Uri(longUrl);
-
-                var hashUrl = Convert.ToBase64String(Encoding.UTF8.GetBytes(longUrl.Remove(6)));
+                //var d = Encoding.UTF8.GetBytes(longUrl.Remove(6));
+                var hashUrl = RandomString(5);
                 var shortUrl = uri.Scheme + "://" + "localhost:7180/api/Home/redirect/" + uri.DnsSafeHost + "/" + hashUrl;
 
                 model.LongUrl = longUrl;
@@ -36,6 +36,13 @@ namespace PruningLink.Services
 
 				throw;
 			}
+        }
+        // Метод для рандомного хеша
+        public string RandomString(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
         }
         //public async Task<Url> ReturnUrlInDBAsync(int id)
         //{
