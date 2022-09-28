9cd9f3d8187e9516690cb6b0d073ad1fe1411bf3
VladislavSoltanov <vsoltanovv@gmail.com>
2022-09-28 08:33:07 +0200

    Добавление создание, удаление и редактирование записей. Добавлена проверка на URl

--- PruningLink/Client/js/script.js
+++ PruningLink/Client/js/script.js
@@ -1,90 +1,134 @@
-const urlGet = 'https://localhost:7180/api/Home/GetShortUrl'
-const urlPost = 'https://localhost:7180/api/Home/CreateShortUrl?longUrl='
-const urlPostDeleate = 'https://localhost:7180/DeletedUrl/'
+const urlGet = 'https://localhost:7180/api/Home/GetShortUrl';
+const urlPostCreate = 'https://localhost:7180/api/Home/CreateShortUrl?longUrl=';
+const urlPostDelete = 'https://localhost:7180/api/Home/DeletedUrl/';
+const urlRefactorPost = 'https://localhost:7180/RefactorUrl?'
 
-let divContainer = document.querySelector('table.listUrl')
+let divContainer = document.querySelector('table.listUrl');
 
-let form = document.querySelector('form')
-let entUrl = document.querySelector('input.ent-url')
-let btnGetData = document.querySelector('button.getData')
-///
-btnGetData.addEventListener('click', async() => {
-    divContainer.innerHTML = ''
-    await GetData(urlGet)
+let inputRefactorUrl = document.querySelector('.refactorUrl');
+let sendRefactorUrl = document.querySelector('.sendRefactorUrl')
+let entUrl = document.querySelector('.sendData')
+let iptUrl = document.querySelector('.ent-url')
 
-})
-///
-function GetData(url, longUrl){
+GetData(urlGet)
+
+function GetData(url){
     fetch(url).then((res) => {
         return res.json()
     }).then((data) =>{
-        // console.log(data)
         data.map((u) => {
+            let createTable = document.createElement('tr')
+            let buttonDeleted = document.createElement('button')
+            let buttonDeletedTd = document.createElement('td')
+            let buttonRefactor = document.createElement('button')
+            let buttonRefactorTd = document.createElement('td')
+            let createLongTd = document.createElement('td')
+            let createShortTd = document.createElement('td')
+            let createCountTd = document.createElement('td')
+            let link = document.createElement('a')
 
-            let createTable = createElement('tr', 'dip')
-            divContainer.appendChild(createTable)
+            createCountTd.classList.add('count')
+            createCountTd.textContent = u.count
 
-            let buttonDeleate = document.createElement('button')
-            buttonDeleate.classList.add(u.id)
+            link.classList.add('link')
+            link.textContent = u.shortUrl
+            link.href = u.shortUrl
 
-            buttonDeleate.addEventListener('click',  async ()=> {
-                console.log(buttonDeleate.className)
-                await deletedUrl(buttonDeleate.className)
+            createShortTd.classList.add('urlShort')
+            createShortTd.appendChild(link)
+            link.addEventListener('click', ()=>{
+                location.reload()
             })
 
-            let buttonTd = document.createElement('td')
-            buttonTd.classList.add('buttonDeleate')
-            buttonTd.appendChild(buttonDeleate)
+            createLongTd.classList.add('urlLong')
+            createLongTd.textContent = u.longUrl
 
-            let createTdShort = document.createElement('td')
-            createTdShort.classList.add('urlShort')
-            createTdShort.textContent = u.shortUrl
+            buttonDeleted.classList.add(u.id)
+            buttonDeleted.textContent = 'Удалить запись'
 
-            let createTdLong = document.createElement('td')
-            createTdLong.classList.add('urlLong')
-            createTdLong.textContent = u.longUrl
+            buttonDeletedTd.classList.add('buttonDeletedUrl')
+            buttonDeletedTd.appendChild(buttonDeleted)
 
-            createTable.append(createTdShort, createTdLong, buttonTd)
+            buttonRefactor.classList.add(u.id)
+            buttonRefactor.textContent = 'Редактировать'
+            buttonRefactorTd.classList.add('buttonRefactorUrl')
+            buttonRefactorTd.appendChild(buttonRefactor)
+
+            buttonRefactor.addEventListener('click', () =>{
+                window.open('RefactorUrl.html')
+            })
+            if (sendRefactorUrl){
+                sendRefactorUrl.addEventListener('click', () =>{
+                    let data = inputRefactorUrl.value
+                    refactorUrl(buttonRefactor.className, data)
+                })
+            }
+
+            if (divContainer){
+                divContainer.appendChild(createTable)
+            }
+            createTable.append(createCountTd, createShortTd, createLongTd, buttonDeletedTd, buttonRefactorTd)
+
+            buttonDeleted.addEventListener('click',async ()=> {
+                await deletedUrl(buttonDeleted.className)
+                alert('Успешно')
+                location.reload()
+            })
         })
+    })
+}
 
+if (entUrl) {
+    entUrl.addEventListener("click",   () => {
+        const objRE = /(^https?:\/\/)?/;
+        console.log(iptUrl.value)
+        if (checkURL(iptUrl.value)){
+            const body = document.getElementsByTagName('input')[0].value
+            sendUrl(body)
+            alert('Успешно!')
+            location.reload()
+        }
+        else{
+            alert('Ошибка')
+        }
+        // Получение данных из input
+
+    });
+}
+function checkURL(url) {
+    const regURL = /^(?:(?:https?|ftp|telnet):\/\/(?:[a-z0-9_-]{1,32}(?::[a-z0-9_-]{1,32})?@)?)?(?:(?:[a-z0-9-]{1,128}\.)+(?:com|net|org|mil|edu|arpa|ru|gov|biz|info|aero|inc|name|[a-z]{2})|(?!0)(?:(?!0[^.]|255)[0-9]{1,3}\.){3}(?!0|255)[0-9]{1,3})(?:\/[a-z0-9.,_@%&?+=\~\/-]*)?(?:#[^ \'\"&<>]*)?$/i;
+    return regURL.test(url);
+}
+// Метод для отправки данных на сервер
+function sendUrl(body){
+    fetch(urlPostCreate + body, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => {
+        return res.json()
     })
 }
 
 function  deletedUrl(id){
-    fetch (urlPostDeleate + id, {
+    fetch (urlPostDelete + id, {
         method: 'POST',
         headers: {
-            'Accept': 'application/json',
             'Content-Type': 'application/json'
         }
     }).then(res => {
-        return res
+        return res.json()
     })
 }
 
-form.addEventListener("submit", (e) => {
-
-    // Получение данных из input
-    const message = document.getElementsByTagName('input')[0].value
-    sendUrl(message)
-
-
-
-})
-// Метод для отправки данных на сервер
-function sendUrl(body){
-    fetch(urlPost + body, {
+function refactorUrl(id, newUrl){
+    fetch (urlRefactorPost + 'id='+ id +'&' + 'url=' + newUrl, {
         method: 'POST',
         headers: {
-            'Accept': 'application/json',
             'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
+        }
+    }).then(res => {
+        return res.json()
     })
-        .then(response => response.json())
-}
-
-function createElement(elementTag, elementClass){
-    let createTable = document.createElement('elementTag')
-    createTable.classList.add('elementClass')
 }
